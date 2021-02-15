2e6bba2aad076d8b1066f100fd882d008591f109
Rod Kay <rodakay5@gmail.com>
2021-02-15 21:30:06 +1100

    Add procedure to run all tests.

---
+++ test/all_tests/test_all.adb
@@ -0,0 +1,30 @@
+with
+     Test_Ashell,
+     Test_Command_Output_To_String,
+     Test_Environment,
+     Test_Iterate_Directory,
+     Test_Piped_Processes,
+     Test_Pipeline_Output,
+     Test_Pipe_Output_To_String,
+     Test_Wait_On_Process,
+     Ada.Text_IO;
+
+procedure Test_All
+is
+   use Ada.Text_IO;
+begin
+   Put_Line ("Start all tests.");
+   New_Line (2);
+
+   Test_Ashell;
+   Test_Command_Output_To_String;
+   Test_Environment;
+   Test_Iterate_Directory;
+   Test_Piped_Processes;
+   Test_Pipeline_Output;
+   Test_Pipe_Output_To_String;
+   Test_Wait_On_Process;
+
+   New_Line (2);
+   Put_Line ("End all tests.");
+end Test_All;
