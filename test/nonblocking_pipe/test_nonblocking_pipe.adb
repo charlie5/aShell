89da7727c4a00bbfb51319e9ff858f799a6a7970
Rod Kay <rodakay5@gmail.com>
2021-03-01 17:03:25 +1100

    Add a test for non-blocking pipes.

---
+++ test/nonblocking_pipe/test_nonblocking_pipe.adb
@@ -0,0 +1,43 @@
+with
+     Shell.Commands,
+     Ada.Text_IO;
+
+procedure Test_Nonblocking_Pipe
+is
+   -- To do this test, run this executable in one terminal, open
+   -- another terminal and start 'cat' (ie  $ cat - >> test.log).
+   -- Then, type any string into the 'cat' terminal. Each entered
+   -- string should then appear in the 'Test_Nonblocking_Pipe' terminal.
+   --
+   -- Entering 'end' on the 'cat' terminal will terminate the test.
+
+   use Ada.Text_IO;
+begin
+   Put_Line ("Begin 'Nonblocking_Pipe' test.");
+   New_Line (2);
+
+   declare
+      use Shell,
+          Shell.Commands;
+      The_Command : Command := To_Command ("tail -f test.log",
+                                           Output => To_Pipe (Blocking => False),
+                                           Errors => To_Pipe (Blocking => False));
+   begin
+      Start (The_Command);
+
+      loop
+         declare
+            Results : constant Command_Results :=  Results_Of (The_Command);
+            Output  : constant String          := +Output_Of  (Results);
+         begin
+            put_line ("'" & Output & "'");
+            delay 1.0;
+            exit when      Output'Length >= 3
+                  and then Output (1..3)  = "end";
+         end;
+      end loop;
+   end;
+
+   New_Line (2);
+   Put_Line ("End 'Nonblocking_Pipe' test.");
+end Test_Nonblocking_Pipe;
