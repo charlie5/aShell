f9aa3008a92f6ee2e110d5792f65ff0e1e72c27c
Rod Kay <rodakay5@gmail.com>
2021-06-10 02:13:28 +1000

    Preliminary work on making spawn manager non-blocking on each command.

--- app/shell-commands-safe-spawn_manager.adb
+++ app/shell-commands-safe-spawn_manager.adb
@@ -1,5 +1,6 @@
 with
      Ada.Text_IO,
+     Ada.Containers.Hashed_Maps,
      Ada.IO_Exceptions,
      Ada.Exceptions;
 
@@ -22,6 +23,19 @@ is
       return True;
    end log;
 
+
+   function Hash (Id : in Safe.Command_Id) return Ada.Containers.Hash_Type
+   is
+   begin
+      return Ada.Containers.Hash_Type (Id);
+   end Hash;
+
+   package Id_Maps_of_Command is new Ada.Containers.Hashed_Maps (Key_Type        => Safe.Command_Id,
+                                                                 Element_Type    => Command,
+                                                                 Hash            => Hash,
+                                                                 Equivalent_Keys =>  "=");
+   Command_Map   : Id_Maps_of_Command.Map;
+
    Input_Stream  : aliased Pipe_Stream := Stream (Shell.Standard_Input);
    Output_Stream : aliased Pipe_Stream := Stream (Shell.Standard_Output);
    Errors_Stream : aliased Pipe_Stream := Stream (Shell.Standard_Error);
@@ -42,16 +56,35 @@ begin
             The_Command :          Command := Forge.To_Command (Input);
             A2          :          Boolean := Log (Image (The_Command)) with Unreferenced;
 
-            Results     : constant Command_Results := Commands.Run (The_Command);
-
-            Output      : constant Data    := Output_Of (Results);
-            A3          :          Boolean := Log (+Output) with Unreferenced;
-
-            Errors      : constant Data    := Errors_Of (Results);
-            A4          :          Boolean := Log (+Errors) with Unreferenced;
          begin
-            Data'Output (Output_Stream'Access, Output);
-            Data'Output (Errors_Stream'Access, Errors);
+            The_Command.Start;
+            Command_Map.Insert (1, The_Command);
+         end;
+
+         declare
+            use Id_Maps_of_Command;
+            Cursor : Id_Maps_of_Command.Cursor := Command_Map.First;
+         begin
+            while Has_Element (Cursor)
+            loop
+               declare
+                  Id          : Command_Id := Key     (Cursor);
+                  The_Command : Command    := Element (Cursor);
+
+                  Results     : constant Command_Results := Results_Of (The_Command);
+
+                  Output      : constant Data    := Output_Of (Results);
+                  A3          :          Boolean := Log (+Output) with Unreferenced;
+
+                  Errors      : constant Data    := Errors_Of (Results);
+                  A4          :          Boolean := Log (+Errors) with Unreferenced;
+               begin
+                  Data'Output (Output_Stream'Access, Output);
+                  Data'Output (Errors_Stream'Access, Errors);
+               end;
+
+               Next (Cursor);
+            end loop;
          end;
 
       exception
