189ba50d37b2d30cc34a78eca00449caa38ffaea
Rod Kay <rodakay5@gmail.com>
2022-12-20 19:07:55 +1100

    tests.core.pause_resume: Use 'ls -R /' as the command to pause and resume.

--- applet/tests/core/pause_resume/test_pause_resume.adb
+++ applet/tests/core/pause_resume/test_pause_resume.adb
@@ -14,52 +14,33 @@ begin
 
    declare
       use Shell;
-
-      --  Sleep : Shell.Process := Start (Program   => "sleep",
-      --                                  --  Arguments => (1 => (+"100000")));
-      --                                  Arguments => (1 => (+"15")));
-
-      Sleep : Shell.Process := Start (Program   => "ls",
+      ls : Shell.Process := Start (Program   => "ls",
                                       Arguments => (1 => (+"-R"),
                                                     2 => (+"/")));
    begin
-      for i in 1 .. 1 -- _000_000
+      for i in 1 .. 5
       loop
-         --  Put_Line ("Pausing  process ('sleep 3') ...");
-         delay 1.02;
-         Put_Line (i'Image & Image (Sleep));
-         Pause (Sleep);
-         delay 3.02;
+         Put_Line (i'Image & Image (ls));
+         delay 2.0;
 
-         --  Put_Line ("a1");
-         --  if not Has_Terminated (Sleep)
-         --  then
-         --     raise Program_Error with "not Has_Terminated:" & i'Image;
-         --  end if;
+         new_Line (2);
+         Put_Line ("Pausing process ('ls -R /') ...");
+         Pause (ls);
+         Put_Line (i'Image & Image (ls));
+         delay 2.0;
 
-         --  Put_Line ("Has terminated: " & Has_Terminated (Sleep)'Image & ".");
-         --  Put_Line ("Normal exit:    " & Normal_Exit    (Sleep)'Image & ".");
-         --  Put_Line ("Exit status:    " & Exit_Status_Of (Sleep.Status))
-
-         Put_Line ("Resuming process ('sleep 3') ...");
-         Resume (Sleep);
-
-         --  Put_Line ("a2");
-         --  delay 0.02;
-
-         --  if Has_Terminated (Sleep)
-         --  then
-         --     raise Program_Error with "Has_Terminated:" & i'Image;
-         --  end if;
-
-         Put_Line ("Waiting");
-         Wait_On (Sleep);
-
-         --  Put_Line ("Normal exit: " & Normal_Exit (Sleep)'Image & ".");
+         Put_Line ("Resuming process ('ls -R /') ...");
+         delay 2.0;
+         Resume (ls);
+         Put_Line (i'Image & Image (ls));
       end loop;
+
+      delay 2.0;
+      New_Line (2);
+      Put_Line ("Killing 'ls -R /' process.");
+      Kill (ls);
    end;
 
-   New_Line (2);
    Put_Line ("End 'Test_Pause_Resume' test.");
 
    shell.Close_Log;
